06191039af437e29df33c192c3f7318e4eb79006
Ac1dburn <colinborins@hotmail.com>
2024-05-31 12:03:24 -0400

    Update catalogupdate.py

--- catalogupdate.py
+++ catalogupdate.py
@@ -16,12 +16,14 @@ def find_app(data, app_name):
                 return result
     return None
 
-def generate_output_structure(test_apps_data, ac1dsworld_apps_data, new_ac1dsworld_apps_data, output_file_path):
+def generate_output_structure(test_apps_data, ac1dsworld_apps_data, new_ac1dsworld_apps_data, premium_apps_data, system_apps_data, output_file_path):
     output_structure = {
         "charts": {},
         "Test": test_apps_data,
         "stable": ac1dsworld_apps_data,
         "ac1dsworld": new_ac1dsworld_apps_data,
+        "premium": premium_apps_data,
+        "system": system_apps_data,
     }
 
     with open(output_file_path, 'w') as output_file:
@@ -39,7 +41,7 @@ with open(catalog_json_path, 'r') as catalog_file:
     catalog_data = json.load(catalog_file)
 
 # App names to read from the "Test" section
-test_app_names_to_read = ["prowlarr", "sonarr", "radarr", "rtorrent-rutorrent", "sabnzbd", "thelounge", "speedtest-exporter","qbittorrent"]
+test_app_names_to_read = ["prowlarr", "sonarr", "radarr", "rtorrent-rutorrent", "sabnzbd", "thelounge", "speedtest-exporter", "qbittorrent"]
 
 # Find the specific app data for each app name in the "Test" section
 test_apps_data = {}
@@ -70,9 +72,26 @@ for app_name in new_ac1dsworld_app_names_to_read:
         app_name_actual, app_data = result
         new_ac1dsworld_apps_data[app_name_actual] = app_data
 
+# Read "premium" section from updated catalog JSON file
+premium_app_names_to_read = ["authelia", "blocky", "clusterissuer", "custom-app", "grafana", "metallb-config", "nextcloud", "prometheus", "traefik", "vaultwarden"]
+premium_apps_data = {}
+for app_name in premium_app_names_to_read:
+    result = find_app(updated_catalog_data["premium"], app_name)
+    if result:
+        app_name_actual, app_data = result
+        premium_apps_data[app_name_actual] = app_data
+
+# Read "system" section from updated catalog JSON file
+system_app_names_to_read = ["cert-manager", "cloudnative-pg", "grafana-agent-operator", "kubeapps", "kubernetes-reflector", "metallb", "openebs", "prometheus-operator", "snapshot-controller", "traefik-crds", "velero", "volsync", "volumesnapshots"]
+system_apps_data = {}
+for app_name in system_app_names_to_read:
+    result = find_app(updated_catalog_data["system"], app_name)
+    if result:
+        app_name_actual, app_data = result
+        system_apps_data[app_name_actual] = app_data
+
 # Output file path
 output_file_path = f"{base_dir}/ac1ds-catalog/catalog.json"
 
 # Generate the output structure and write to file
-generate_output_structure(test_apps_data, ac1dsworld_apps_data, new_ac1dsworld_apps_data, output_file_path)
-
+generate_output_structure(test_apps_data, ac1dsworld_apps_data, new_ac1dsworld_apps_data, premium_apps_data, system_apps_data, output_file_path)
