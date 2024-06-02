909b643c76f09a5c913f5931436ee056eab08d76
ac1dburn <ac1d@fuckyoukaren.ca>
2024-06-02 18:41:20 -0400

    modified:   app_versions_fix-script.py 	modified:   premium/prometheus/app_versions.json

--- app_versions_fix-script.py
+++ app_versions_fix-script.py
@@ -2,10 +2,19 @@ import os
 import json
 from packaging import version
 
-base_dir = "/Users/ac1dburn/Documents/GitHub"
-ignored_apps = ["prowlarr", "radarr", "rtorrent-rutorrent", "sabnzbd", "sonarr", "speedtest-exporter", "thelounge", "traefik"]
+base_dir = "/Users/ac1dburn/Documents/GitHub/ac1ds-catalog"
+ignored_apps = ["prowlarr", "rtorrent-rutorrent", "sabnzbd", "sonarr", "speedtest-exporter", "thelounge", "traefik"]
+ignored_dirs = ["mainfiles"]
 
-def find_highest_version(app_versions):
+def find_highest_version(app_dir):
+    app_versions_path = os.path.join(app_dir, "app_versions.json")
+    if os.path.exists(app_versions_path):
+        with open(app_versions_path, "r") as file:
+            app_versions = json.load(file)
+            return find_highest_version_from_dict(app_versions)
+    return ""
+
+def find_highest_version_from_dict(app_versions):
     highest_version = ""
     for version_str in app_versions.keys():
         try:
@@ -29,7 +38,7 @@ def update_app_versions_json(app_dir, app_name):
     else:
         app_versions = {}
 
-    highest_version = find_highest_version(app_versions)
+    highest_version = find_highest_version_from_dict(app_versions)
     if highest_version:
         new_version = increment_version(highest_version)
         latest_version_data = app_versions[highest_version]
@@ -41,12 +50,21 @@ def update_app_versions_json(app_dir, app_name):
             json.dump(app_versions, file, indent=2)
 
         print(f"Updated app_versions.json for {app_name} with new version {new_version}")
+    else:
+        print(f"No versions found in the app_versions.json file for {app_name}")
 
 def main():
-    for app_dir in os.listdir(base_dir):
-        app_dir_path = os.path.join(base_dir, app_dir)
-        if os.path.isdir(app_dir_path) and app_dir not in ignored_apps:
-            update_app_versions_json(app_dir_path, app_dir)
+    for dir_name in os.listdir(base_dir):
+        parent_dir_path = os.path.join(base_dir, dir_name)
+        if os.path.isdir(parent_dir_path) and dir_name not in ignored_dirs:
+            for sub_dir_name in os.listdir(parent_dir_path):
+                sub_dir_path = os.path.join(parent_dir_path, sub_dir_name)
+                if os.path.isdir(sub_dir_path) and sub_dir_name not in ignored_apps:
+                    app_dir_path = os.path.join(sub_dir_path)
+                    versions_dir = os.path.join(app_dir_path, find_highest_version(app_dir_path))
+                    if versions_dir:
+                        print(f"Processing app directory: {sub_dir_name}")
+                        update_app_versions_json(versions_dir, sub_dir_name)
 
 if __name__ == "__main__":
     main()
